3c4de81a8f1bd07b7c47814f277fb1ee04de6f96
Ryumin Sergey <90983639+ryu-min@users.noreply.github.com>
2025-02-01 16:16:56 +0300

    update service

--- cnn/service.py
+++ cnn/service.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import chess
 import torch
-from cnn import ChessCNN, load_model, predict_move
+from cnn import ChessCNN, load_model, predict_move, ChessCNN_New
 import argparse
 from flask_cors import CORS
 
@@ -10,7 +10,7 @@ CORS(app)
 
 # Определяем функцию для загрузки модели
 def load_chess_model(model_path):
-    model = ChessCNN()
+    model = ChessCNN_New()
     optimizer = torch.optim.Adam(model.parameters())
     load_model(model, optimizer, model_path)
     return model
@@ -19,26 +19,32 @@ def load_chess_model(model_path):
 def get_best_move():
     data = request.json
     fen = data.get('fen')
-    
+    print(fen)
+
     if not fen:
         return jsonify({'error': 'FEN string is required'}), 400
+
+    if fen == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1":
+        return jsonify({'best_move': "e2e4"})
 
     board = chess.Board(fen)
     best_move = predict_move(model, board, depth)
 
     if best_move:
+        print(f"move is {best_move.uci()}")
         return jsonify({'best_move': best_move.uci()})
     else:
         return jsonify({'error': 'No legal moves available'}), 400
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Запуск REST-сервиса для шахматного AI")
-    parser.add_argument('model_file', type=str, help='Path to the saved model file')
-    depth = 4
+    # parser = argparse.ArgumentParser(description="Запуск REST-сервиса для шахматного AI")
+    # parser.add_argument('model_file', type=str, help='Path to the saved model file')
+    model_file =  ".\small_model_300k_epoch_50.pt"
+    depth = 3
     
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
     # Загружаем модель с указанного пути
-    model = load_chess_model(args.model_file)
+    model = load_chess_model(model_file)
 
     app.run(debug=True)
